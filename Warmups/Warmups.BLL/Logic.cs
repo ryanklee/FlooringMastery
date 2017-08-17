b8dfc564cd2ec303e37596f2f7ed467999368f13
Ryan Kleeberger <rylklee@gmail.com>
2017-08-17 09:24:02 -0500

    RYKLINWRK-7 finished string exercise

---
+++ Warmups/Warmups.BLL/Logic.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warmups.BLL
+{
+    public class Logic
+    {
+
+        public bool GreatParty(int cigars, bool isWeekend)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public int CanHazTable(int yourStyle, int dateStyle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool PlayOutside(int temp, bool isSummer)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public int CaughtSpeeding(int speed, bool isBirthday)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public int SkipSum(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public string AlarmClock(int day, bool vacation)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public bool LoveSix(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public bool InRange(int n, bool outsideMode)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public bool SpecialEleven(int n)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public bool Mod20(int n)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public bool Mod35(int n)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public bool AnswerCell(bool isMorning, bool isMom, bool isAsleep)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public bool TwoIsOne(int a, int b, int c)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public bool AreInOrder(int a, int b, int c, bool bOk)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public bool InOrderEqual(int a, int b, int c, bool equalOk)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public bool LastDigit(int a, int b, int c)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public int RollDice(int die1, int die2, bool noDoubles)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
