99a8a007376ee54987a33ab58a3e66292ddf4115
Ryan Kleeberger <rylklee@gmail.com>
2017-09-05 18:59:21 -0500

    factorfinger class.

---
+++ BTFactorizor/Factorizor.BLL/FactorFinder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorizor.BLL
+{
+    public class FactorFinder
+    {
+        private List<int> _factors = new List<int>();
+
+        private void GetFactors(int number)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                if (number % i == 0)
+                    _factors.Add(i);
+            }
+        }
+
+        public List<int> ReturnFactors()
+        {
+            return _factors;
+        }
+    }
+}
