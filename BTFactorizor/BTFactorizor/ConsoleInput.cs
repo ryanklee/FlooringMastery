5dca3073312a4c4ca3b94dc69ca08bbb90cb0bf1
Ryan Kleeberger <rylklee@gmail.com>
2017-09-05 22:46:29 -0500

    input validation

---
+++ BTFactorizor/BTFactorizor/ConsoleInput.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTFactorizor
+{
+    class ConsoleInput
+    {
+        public int NumberToFactorize { get; private set; }
+
+        private int GetNumber()
+        {
+            int number;
+
+            while (true)
+            {
+                Console.WriteLine("Enter integer to factorize: ");
+                string input = Console.ReadLine();
+
+                if (Int32.TryParse(input, out number))
+                    break;
+                else
+                    Console.WriteLine("That was not an integer!");
+            }
+
+            return number;
+        }
+
+    }
+}
