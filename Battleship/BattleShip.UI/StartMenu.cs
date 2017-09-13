ce8fc081e391bb3e795a761649dda1656c3c8c2f
Ryan Kleeberger <rylklee@gmail.com>
2017-09-12 21:43:57 -0500

    queried, stored player names

--- Battleship/BattleShip.UI/StartMenu.cs
+++ Battleship/BattleShip.UI/StartMenu.cs
@@ -8,10 +8,38 @@ namespace BattleShip.UI
 {
     public class StartMenu
     {
+        public string P1Name { get; private set; }
+        public string P2Name { get; private set; }
+
         public void Menu()
         {
+            MenuIntro();
+            GetP1Name();
+            GetP2Name();
+            Greeting();
+            Console.ReadLine();
+        }
+
+        private void MenuIntro()
+        {
             Console.WriteLine("MAIN MENU");
-            Console.Read();
+        }
+
+        private void GetP1Name()
+        {
+            Console.WriteLine("Enter P1 Name: ");
+            P1Name = Console.ReadLine();
+        }
+
+        private void GetP2Name()
+        {
+            Console.WriteLine("Enter P2 Name: ");
+            P2Name = Console.ReadLine();
+        }
+
+        private void Greeting()
+        {
+            Console.WriteLine("Hello {0} and {1}. Wecome to Battleship!", P1Name, P2Name);
         }
     }
 }
