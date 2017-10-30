fab027cf814f26376b47850bd0f9439109060af9
Ryan Kleeberger <rylklee@gmail.com>
2017-10-29 20:18:27 -0500

    RYKLINWRK-11 task 8 complete

--- SGBank/SGBank.Tests/FreeAccountTests.cs
+++ SGBank/SGBank.Tests/FreeAccountTests.cs
@@ -1,6 +1,9 @@
 ﻿using NUnit.Framework;
 using SGBank.BLL;
+using SGBank.BLL.DepositRules;
+using SGBank.BLL.WithdrawRules;
 using SGBank.Models;
+using SGBank.Models.Interfaces;
 using SGBank.Models.Responses;
 using System;
 using System.Collections.Generic;
@@ -24,5 +27,48 @@ namespace SGBank.Tests
             Assert.IsTrue(response.Success);
             Assert.AreEqual("12345", response.Account.AccountNumber);
         }
+
+        [Test]
+        [TestCase("12345", "Free Account", 100, AccountType.Free, 250, false)]
+        [TestCase("12345", "Free Account", 100, AccountType.Free, -100, false)]
+        [TestCase("12345", "Free Account", 100, AccountType.Basic, 50, false)]
+        [TestCase("12345", "Free Account", 100, AccountType.Free, 50, true)]
+        public void FreeAccountDepositRuleTest(string accountNumber, string name, decimal balance, AccountType accountType, decimal amount, bool expectedResult)
+        {
+            IDeposit deposit = new FreeAccountDepositRule();
+            Account account = new Account
+            {
+                AccountNumber = accountNumber,
+                Name = name,
+                Balance = balance,
+                Type = accountType
+            };
+
+            AccountDepositResponse response = deposit.Deposit(account, amount);
+
+            Assert.AreEqual(response.Success, expectedResult);
+        }
+
+        [Test]
+        [TestCase("12345", "Free Account", 100, AccountType.Free, 50, false)]
+        [TestCase("12345", "Free Account", 100, AccountType.Free, -200, false)]
+        [TestCase("12345", "Free Account", 100, AccountType.Basic, -50, false)]
+        [TestCase("12345", "Free Account", 100, AccountType.Free, -50, true)]
+        public void FreeAccountWithdrawRuleTest(string accountNumber, string name, decimal balance, AccountType accountType, decimal amount, bool expectedResult)
+        {
+            IWithdraw withdraw = new FreeAccountWithdrawRule();
+            Account account = new Account
+            {
+                AccountNumber = accountNumber,
+                Name = name,
+                Balance = balance,
+                Type = accountType
+            };
+
+            AccountWithdrawResponse response = withdraw.Withdraw(account, amount);
+
+            Assert.AreEqual(response.Success, expectedResult);
+        }
     }
 }
+
