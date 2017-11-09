4b3711c052b23ac30c4ec6e4cde5d168ea37ee7f
Ryan Kleeberger <rylklee@gmail.com>
2017-11-09 14:53:37 -0600

    RYKLINWRK-12 menu created

---
+++ FlooringMastery/FlooringMastery/Menu.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlooringMastery
+{
+    public class Menu
+    {
+        public static void Start()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Flooring Program");
+                Console.WriteLine("------------------------");
+                Console.WriteLine("1. Display Orders");
+                Console.WriteLine("2. Add an Order");
+                Console.WriteLine("3. Edit an Order");
+                Console.WriteLine("4. Remove an Order");
+
+                Console.WriteLine("\nQ to quit");
+                Console.Write("\nEnter selection: ");
+
+                string userinput = Console.ReadLine();
+
+                switch (userinput)
+                {
+                    case "1":
+                        throw new NotImplementedException();
+                    case "2":
+                        throw new NotImplementedException();
+                    case "3":
+                        throw new NotImplementedException();
+                    case "Q":
+                        return;
+                }
+
+            }
+
+        }
+    }
+}
--- FlooringMastery/FlooringMastery/Program.cs
+++ FlooringMastery/FlooringMastery/Program.cs
@@ -10,6 +10,7 @@ namespace FlooringMastery
     {
         static void Main(string[] args)
         {
+            Menu.Start();
         }
     }
 }
